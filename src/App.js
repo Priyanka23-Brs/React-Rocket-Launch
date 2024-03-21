742510a3a89d7e70bd1a45bd6535412e03b6f12f
Priyanka23-Brs <priya2000509@gmail.com>
2024-03-21 15:59:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RocketLaunch from './RocketLaunch';
+
+function App() {
+  return (
+    <div className="App">
+      <RocketLaunch />
+    </div>
+  );
+}
+
+export default App;
